bb063db2e6a38344d1c14a92ab2fda67ebd76484
Rafaela Justino <rafaela.justino@pixter.com.br>
2023-04-09 09:18:02 -0300

    change color subdivisao

--- components/subdivisao.jsx
+++ components/subdivisao.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Subdivisao.module.css'
 export default function Subdivisao(props) {
     return (
      <div 
-     style={{ backgroundColor: props.preto ? "#000" : "#333" }}
+     style={{ backgroundColor: props.preto ? "#000" : "#D22" }}
      className={styles.subdivisao}>
      </div>
     )
